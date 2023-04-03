a242d9bdd5b45c1a814b2424be533c0a5e2c0c71
Elidon Morina <83674499+3mper0r@users.noreply.github.com>
2023-04-04 00:26:10 +0200

    fix: navbar icon positions

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,13 +8,17 @@ const Navbar = () => {
 
     return (
         <>
-            <nav className='flex flex-row-'>
-            <GiHamburgerMenu/>
-            <span className='logo'>BeastWear</span>
-            <FiSearch/>
-            <RiUser3Line />
-            <AiOutlineHeart/>
-            <AiOutlineShoppingCart/>
+            <nav className='grid grid-flow-col items-center bg-black p-2'>
+                <div className='flex items-center'>
+                    <GiHamburgerMenu className='text-white'/>
+                    <h1 className='logo text-white ml-2'><span className='logo text-red-500'>Beast</span> Wear</h1>
+                </div>
+                <div className='grid grid-flow-col justify-end icons'>
+                    <FiSearch className='mr-4 text-white'/>
+                    <RiUser3Line className='mr-2 text-white'/>
+                    <AiOutlineHeart className='mr-2 text-white'/>
+                    <AiOutlineShoppingCart className='mr-2 text-white'/>    
+                </div>
             </nav>             
         </>
     )
